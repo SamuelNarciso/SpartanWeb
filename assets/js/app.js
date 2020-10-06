2f579604828b00fb2030c6278bb2502ced117424
Samuel Narciso <SamuelNarciso3280@gmail.com>
2020-10-05 21:44:40 -0500

    conecte javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,29 @@
 const db = firebase.firestore();
 const add_user_button = document.querySelector('#add_user');
+const RegistroUsuario_ventana = document.querySelector('.RegistroUsuario');
 
+const cerrar_registro_button = document.querySelector('#cerrar_registro');
 
+const nuevo_usuario_Eliminar = document.querySelector(
+	'#nuevo_usuario_Eliminar'
+);
+const nuevo_usuario_Copiar = document.querySelector('#nuevo_usuario_Copiar');
+const nuevo_usuario_Editar = document.querySelector('#nuevo_usuario_Editar');
+const nuevo_usuario_Guardar = document.querySelector('#nuevo_usuario_Guardar');
 
 add_user_button.addEventListener('click', () => {
-console.log('Boton presionado');
+	const RegistroUsuario = document.querySelector('.RegistroUsuario');
+	RegistroUsuario.classList.toggle('expand');
+});
 
+cerrar_registro_button.addEventListener('click', () => {
+	const RegistroUsuario = document.querySelector('.RegistroUsuario');
+	RegistroUsuario.classList.toggle('expand');
+});
+
+nuevo_usuario_Guardar.addEventListener('click', () => {
+    const campos = document.querySelectorAll('.camposRegistro .campo');
+    const [nombre,telefono,direccion,ubicacion,entreCalle1,entreCalle2,comentariosExtra] = campos;
+    console.log(nombre.value);
+    
 });
