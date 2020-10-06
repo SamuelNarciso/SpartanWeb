9fbf1a316d407361d81f2491f89af79ed4e4aa16
Samuel Narciso <SamuelNarciso3280@gmail.com>
2020-10-05 19:44:53 -0500

    ya hice el fontend, ahora estoy comenzando con el formulario de nuevo usuario y js, ya conecte con firebase

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+const db = firebase.firestore();
+const add_user_button = document.querySelector('#add_user');
+
+
+
+add_user_button.addEventListener('click', () => {
+console.log('Boton presionado');
+
+});
